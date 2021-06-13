b49c007da8f08b1863fe2a09d0895ab15102ebba
dylan <dylanliner@gmail.com>
2021-06-13 18:30:08 +0200

    add facebook exercises

--- src/main/java/RestoreIPAddresses.java
+++ src/main/java/RestoreIPAddresses.java
@@ -3,13 +3,64 @@ import java.util.List;
 
 public class RestoreIPAddresses {
     public static void main(String[] args) {
+        System.out.println(restoreIpAddresses("101023"));
         System.out.println(restoreIpAddresses("25525511135"));
+        System.out.println(restoreIpAddresses("0000"));
 
     }
 
     public static List<String> restoreIpAddresses(String s) {
 
-        return new ArrayList<>();
+        if(s.length()>16){
+            return new ArrayList<>();
+        }
+
+        List<String> mainList = new ArrayList<>();
+        char[] charArray = s.toCharArray();
+
+
+        addNumber(new StringBuilder(), charArray, 0, mainList, 0, 0);
+        mainList.removeIf(el -> !isValid(el));
+
+        return mainList;
 
     }
+
+    private static boolean isValid(String string) {
+
+        String[] stringArr = string.split("\\.");
+
+        for (String s : stringArr) {
+            if ((s.length() > 1 && s.charAt(0) == '0') || Integer.parseInt(s) > 255) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void addNumber(StringBuilder prevSb, char[] charArray, int i, List<String> mainList, int pointCount, int lastPointIndex) {
+        if (pointCount == 3 && i == charArray.length) {
+            mainList.add(prevSb.toString());
+        }
+        if (i < charArray.length) {
+
+
+            if (i - lastPointIndex < 3) {
+                StringBuilder sb2 = new StringBuilder(prevSb);
+                sb2.append(charArray[i]);
+                addNumber(sb2, charArray, i + 1, mainList, pointCount, lastPointIndex);
+            }
+
+            if (i - lastPointIndex >= 1 && i - lastPointIndex < 4) {
+                StringBuilder sb3 = new StringBuilder(prevSb);
+                sb3.append('.');
+                addNumber(sb3, charArray, i, mainList, pointCount + 1, i);
+            }
+
+
+        }
+    }
+
+
 }
---
+++ src/main/java/RussianDollEnvelopes.java
@@ -0,0 +1,11 @@
+public class RussianDollEnvelopes {
+
+    public static void main(String[] args) {
+        System.out.println(maxEnvelopes(new int[][]{{5, 4}, {6, 4}, {6, 7}, {2, 3}}));
+
+    }
+
+    public static int maxEnvelopes(int[][] envelopes) {
+        return 0;
+    }
+}
