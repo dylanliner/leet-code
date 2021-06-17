b3175f38f7ec7d5b64052cad86297a2eb7e4904d
Dylan <dylanliner@gmail.com>
2021-06-18 00:27:55 +0200

    add LIS-related exercices

--- src/main/java/LongestIncreasingSubsequence.java
+++ src/main/java/LongestIncreasingSubsequence.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class LongestIncreasingSubsequence {
 
     public static void main(String[] args) {
 
-//        System.out.println(lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+        System.out.println(lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
         System.out.println(lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}));
         System.out.println(lengthOfLIS(new int[]{4, 10, 4, 3, 8, 9}));
 
@@ -12,18 +13,24 @@ public class LongestIncreasingSubsequence {
     }
 
     public static int lengthOfLIS(int[] nums) {
-        return lengthofLIS(nums, Integer.MIN_VALUE, 0);
+
+        int dp[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < current) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                    }
+
+                }
+            }
+        }
+        return Arrays.stream(dp).max().getAsInt();
     }
 
-    public static int lengthofLIS(int[] nums, int prev, int curpos) {
-        if (curpos == nums.length) {
-            return 0;
-        }
-        int taken = 0;
-        if (nums[curpos] > prev) {
-            taken = 1 + lengthofLIS(nums, nums[curpos], curpos + 1);
-        }
-        int nottaken = lengthofLIS(nums, prev, curpos + 1);
-        return Math.max(taken, nottaken);
-    }
 }
--- src/main/java/RussianDollEnvelopes.java
+++ src/main/java/RussianDollEnvelopes.java
@@ -4,96 +4,40 @@ import java.util.stream.Collectors;
 public class RussianDollEnvelopes {
 
     public static void main(String[] args) {
-        System.out.println(maxEnvelopes2(new int[][]{{5, 4}, {6, 4}, {6, 7}, {2, 3}}));
-        System.out.println(maxEnvelopes2(new int[][]{{1, 1}, {1, 1}, {1, 1}}));
-        System.out.println(maxEnvelopes2(new int[][]{{2, 100}, {3, 200}, {4, 300}, {5, 500}, {5, 400}, {5, 250}, {6, 370}, {6, 360}, {7, 380}}));
+        System.out.println(maxEnvelopes(new int[][]{{5, 4}, {6, 4}, {6, 7}, {2, 3}}));
+        System.out.println(maxEnvelopes(new int[][]{{1, 1}, {1, 1}, {1, 1}}));
+        System.out.println(maxEnvelopes(new int[][]{{2, 100}, {3, 200}, {4, 300}, {5, 500}, {5, 400}, {5, 250}, {6, 370}, {6, 360}, {7, 380}}));
 
     }
 
     public static int maxEnvelopes(int[][] envelopes) {
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        int count = 0;
-        for (int i = 0; i < envelopes.length; i++) {
-            pq.add(envelopes[i][0]);
-            map.computeIfAbsent(envelopes[i][0], k -> new ArrayList<>()).add(envelopes[i][1]);
-        }
+        //Sort By w
 
-        int prevHeight = 0;
-        int prevWidth = 0;
-        while (!pq.isEmpty()) {
-            int width = pq.poll();
-            int finalPrevHeight = prevHeight;
-            Collections.sort(map.get(width));
-            if (prevWidth < width && map.get(width).stream().anyMatch(height -> height > finalPrevHeight)) {
-                count++;
-                prevWidth = width;
-                if (map.get(width).stream().anyMatch(height -> height > finalPrevHeight)) {
-                    prevHeight = map.get(width).stream().filter(height -> height > finalPrevHeight).collect(Collectors.toList()).get(0);
-                } else {
-                    prevHeight = map.get(width).get(0);
+
+        Arrays.sort(envelopes, (o1, o2) -> {
+            if (o2[0] == o1[0]) {
+                return o2[1] - o1[1];
+            }
+            return o1[0] - o2[0];
+        });
+        //if w are equals but the highest h first (because we want to fail on those when doing LIS)
+        //Do LIS on h
+
+        int[] dp = new int[envelopes.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < envelopes.length; i++) {
+            int current = envelopes[i][1];
+            for (int j = 0; j < i; j++) {
+                if ( envelopes[j][1]<current) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                    }
                 }
             }
 
-
         }
 
-
-        return count;
-    }
-
-    public static int maxEnvelopes2(int[][] envelopes) {
-
-        List<Integer> list = new ArrayList<>();
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        int maxCount = 0;
-        for (int i = 0; i < envelopes.length; i++) {
-            list.add(envelopes[i][0]);
-            map.computeIfAbsent(envelopes[i][0], k -> new ArrayList<>()).add(envelopes[i][1]);
-        }
-
-        map.forEach((Integer i, List<Integer> heights) -> {
-            Collections.sort(heights);
-        });
-
-        Collections.sort(list);
-        for (int i = 0; i < list.size(); i++) {
-            int count = 0;
-            int width = list.get(i);
-            int height = map.get(width).get(0);
-            count = checkForSmaller(width, height, list, map, i - 1, 0) + checkForLarger(width, height, list, map, i + 1, 0) + 1;
-            ;
-            if (count > maxCount) {
-                maxCount = count;
-            }
-        }
-
-
-        return maxCount;
-    }
-
-    private static int checkForLarger(int width, int height, List<Integer> list, HashMap<Integer, List<Integer>> map, int i, int count) {
-
-        if (i < 0 || i == list.size()) {
-            return count;
-        }
-        if (list.get(i) > width && map.get(list.get(i)).stream().anyMatch(x -> x > height)) {
-            return checkForLarger(list.get(i), map.get(list.get(i)).stream().filter(x -> x > height).collect(Collectors.toList()).get(0), list, map, i + 1, count + 1);
-        } else {
-            return checkForLarger(width, height, list, map, i + 1, count);
-        }
-    }
-
-    private static int checkForSmaller(int width, int height, List<Integer> list, HashMap<Integer, List<Integer>> map, int i, int count) {
-
-        if (i < 0 || i == list.size()) {
-            return count;
-        }
-        if (list.get(i) < width && map.get(list.get(i)).stream().anyMatch(x -> x < height)) {
-                return checkForSmaller(list.get(i), map.get(list.get(i)).stream().filter(x -> x < height).collect(Collectors.toList()).get(map.get(list.get(i)).stream().filter(x -> x < height).collect(Collectors.toList()).size() - 1), list, map, i - 1, count + 1);
-        } else {
-            return checkForSmaller(width, height, list, map, i - 1, count);
-        }
+        return Arrays.stream(dp).max().getAsInt();
     }
 }
