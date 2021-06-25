2e650056db52e9bfe9665c283030c77c9cccda9a
dylan <dylanliner@gmail.com>
2021-06-26 00:50:51 +0200

    add facebook exercises

--- src/main/java/MinimumRemoveToMakeValidParentheses.java
+++ src/main/java/MinimumRemoveToMakeValidParentheses.java
@@ -1,14 +1,56 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
+
 public class MinimumRemoveToMakeValidParentheses {
 
     public static void main(String[] args) {
-
+//11:55
         System.out.println(minRemoveToMakeValid("lee(t(c)o)de)"));
+        System.out.println(minRemoveToMakeValid("a)b(c)d"));
+        System.out.println(minRemoveToMakeValid("))(("));
+        System.out.println(minRemoveToMakeValid("(a(b(c)d)"));
+        System.out.println(minRemoveToMakeValid("())()((("));
 
 
     }
 
 
     public static String minRemoveToMakeValid(String s) {
-        return s;
+
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (currentChar == '(') {
+                stack.add(currentChar);
+                sb.append(currentChar);
+            } else if (currentChar == ')') {
+                if (!stack.isEmpty()) {
+                    Character lastChar = stack.peek();
+                    if (lastChar == '(') {
+                        stack.pop();
+                        sb.append(currentChar);
+
+                    }
+                }
+            } else {
+                sb.append(currentChar);
+            }
+
+
+        }
+
+        for (int i = sb.length()-1; !stack.isEmpty(); i--) {
+            if (sb.charAt(i) == '(') {
+                sb.deleteCharAt(i);
+                stack.pop();
+            }
+        }
+
+
+        return sb.toString();
     }
 }
