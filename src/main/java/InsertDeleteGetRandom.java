73f48820655491ed4d54500e74c67411b7b7436b
dylan <dylanliner@gmail.com>
2021-06-25 21:51:59 +0200

    add facebook exercises

--- src/main/java/InsertDeleteGetRandom.java
+++ src/main/java/InsertDeleteGetRandom.java
@@ -4,12 +4,20 @@ public class InsertDeleteGetRandom {
 
         RandomizedCollection randomizedCollection = new RandomizedCollection();
 //10:40
-        System.out.println(randomizedCollection.insert(0));
+//        System.out.println(randomizedCollection.insert(4));
+//        System.out.println(randomizedCollection.insert(3));
+//        System.out.println(randomizedCollection.insert(4));
+//        System.out.println(randomizedCollection.insert(2));
+//        System.out.println(randomizedCollection.insert(4));
+//        System.out.println(randomizedCollection.remove(4));
+//        System.out.println(randomizedCollection.remove(3));
+//        System.out.println(randomizedCollection.remove(4));
+//        System.out.println(randomizedCollection.remove(4));
+//        System.out.println(randomizedCollection.getRandom());
+
         System.out.println(randomizedCollection.insert(1));
-        System.out.println(randomizedCollection.remove(0));
-        System.out.println(randomizedCollection.insert(2));
         System.out.println(randomizedCollection.remove(1));
-        System.out.println(randomizedCollection.getRandom());
+        System.out.println(randomizedCollection.insert(1));
 
     }
 
--- src/main/java/RandomizedCollection.java
+++ src/main/java/RandomizedCollection.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class RandomizedCollection {
 
 
-    private HashMap<Integer, Stack<Integer>> mapOfIndices = new HashMap<>();
+    private HashMap<Integer, Set<Integer>> mapOfIndices = new HashMap<>();
     private ArrayList<Integer> list = new ArrayList<>();
-    private int offset = 0;
 
     /**
      * Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
@@ -13,11 +12,11 @@ public class RandomizedCollection {
     public boolean insert(int val) {
         if (!mapOfIndices.containsKey(val)) {
             list.add(val);
-            mapOfIndices.computeIfAbsent(val, x -> new Stack<>()).add(list.size() - 1);
+            mapOfIndices.computeIfAbsent(val, x -> new HashSet<>()).add(list.size() - 1);
             return true;
         } else {
             list.add(val);
-            mapOfIndices.computeIfAbsent(val, x -> new Stack<>()).add(list.size() - 1);
+            mapOfIndices.computeIfAbsent(val, x -> new HashSet<>()).add(list.size() - 1);
             return false;
         }
     }
@@ -28,11 +27,21 @@ public class RandomizedCollection {
     public boolean remove(int val) {
 
         if (mapOfIndices.containsKey(val)) {
-            list.remove((int) mapOfIndices.get(val).pop());
-            offset = offset + 1;
+            int lastItem = list.get(list.size() - 1);
+            int swapIndex = mapOfIndices.get(val).iterator().next();
+            mapOfIndices.get(val).remove(swapIndex);
+            list.set(swapIndex, lastItem);
+
             if (mapOfIndices.get(val).size() == 0) {
                 mapOfIndices.remove(val);
             }
+            if (mapOfIndices.get(lastItem) != null) {
+                mapOfIndices.get(lastItem).add(swapIndex);
+                mapOfIndices.get(lastItem).remove(list.size() - 1);
+
+            }
+
+            list.remove(list.size() - 1);
             return true;
         }
         return false;
