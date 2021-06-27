e720572829c2b53d0d319552b4525380c73f09b8
dylan <dylanliner@gmail.com>
2021-06-27 16:37:59 +0200

    add facebook exercises

---
+++ src/main/java/MaxConsecutiveOnesIII.java
@@ -0,0 +1,18 @@
+public class MaxConsecutiveOnesIII {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(longestOnes(new int[]{1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0}, 2));
+
+//        12:10
+    }
+
+    public static int longestOnes(int[] nums, int k) {
+
+
+        return k;
+    }
+
+
+}
